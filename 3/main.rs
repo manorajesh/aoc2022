0dfeee379a016b75b6ad3702d287754f0d88d96b
manorajesh <mano@raje.sh>
2023-01-01 20:59:02 -0800

    Second half of Day 3 done

--- 3/main.rs
+++ 3/main.rs
@@ -1,25 +1,46 @@
 use std::fs::read_to_string;
+use std::str::Lines;
 
 fn main() {
     let input = read_to_string("input-3.txt").unwrap();
     let rucksacks = input.lines();
-    let mut total_priorities = 0;
+    let mut total_priorities: usize = 0;
 
-    for rucksack in rucksacks {
-        let compart1 = &rucksack[0..rucksack.len()/2];
-        let compart2 = &rucksack[rucksack.len()/2..rucksack.len()];
-
-        let mut shared: char = ' ';
-        for char in compart1.chars() {
-            if compart2.contains(char) {
-                shared = char;
-            }
-        }
+    let groups_of_3 = groups_of(rucksacks, 3);
+    for group in groups_of_3 {
+        let shared = find_shared_char(group[0], group[1], group[2]);
 
         match shared.is_lowercase() {
-            true => total_priorities += (shared as u8 - 96) as i32,
-            false => total_priorities += (shared as u8 - 38) as i32,
+            true => total_priorities += (shared as u8 - 96) as usize,
+            false => total_priorities += (shared as u8 - 38) as usize,
         }
     }
     println!("{}", total_priorities);
+}
+
+fn groups_of(rucksacks: Lines, group_size: usize) -> Vec<Vec<&str>> {
+    let mut groups = Vec::new();
+    let mut index = 1;
+    let mut group = Vec::new();
+    for rucksack in rucksacks {
+        group.push(rucksack);
+        if index > group_size-1 {
+            groups.push(group);
+            group = Vec::new();
+            index = 0;
+        }
+        index += 1;
+    }
+    groups
+}
+
+fn find_shared_char(string1: &str, string2: &str, string3: &str) -> char {
+    let mut shared = ' ';
+    for char in string1.chars() {
+        if string2.contains(char) && string3.contains(char) {
+            shared = char;
+            break;
+        }
+    }
+    shared
 }
