ce81c6e351a59e0ee7fa3a098da2c5a6d1698697
manorajesh <mano@raje.sh>
2023-01-11 14:31:12 -0800

    Finished second half of Day 10

--- 10/main.rs
+++ 10/main.rs
@@ -5,42 +5,63 @@ fn main() {
     let lines = input.lines();
     let mut X = 1; // register
     let mut cycles = 0;
-    let mut signal_strengths = Vec::new();
+    let mut crt_frame_buffer = Vec::with_capacity(240);
 
-    let mut pause_at: usize = 20;
+    let mut pause_at: usize = 0;
 
     for inst in lines {
         let op = inst.split(" ").nth(0).unwrap();
 
         match op {
-            "noop" => signal_strengths.push(wait(1, &mut cycles, X, pause_at)),
+            "noop" => draw(1, &mut cycles, X, &mut crt_frame_buffer),
             "addx" => {
-                signal_strengths.push(wait(2, &mut cycles, X, pause_at)); 
+                draw(2, &mut cycles, X, &mut crt_frame_buffer); 
                 X += inst.split(" ").nth(1).unwrap().parse::<i32>().unwrap();
             },
             _ => panic!("Unknown instruction: {}", op),
         }
-        if pause_at > 220 {
+        if pause_at > 240 {
             break;
         } else if cycles >= pause_at {
             pause_at += 40;
         }
+        // clear_screen();
+        // print_crt(&crt_frame_buffer);
     }
-    println!("Signal Strengths: {:?}", signal_strengths.iter().sum::<i32>());
+    print_crt(&crt_frame_buffer);
 }
 
-fn wait(wait_cycles: usize, current_cycle: &mut usize, reg_val: i32, pause_cycle: usize) -> i32 {
-    let mut signal_strength = 0;
+fn draw(wait_cycles: usize, current_cycle: &mut usize, reg_val: i32, crt_frame_buffer: &mut Vec<char>) {
     for _ in 1..=wait_cycles {
-        if *current_cycle == pause_cycle {
-            signal_strength = pause_cycle as i32 * reg_val;
-            println!("Signal Strength: {}; Cycle Count: {}; X = {}", signal_strength, &current_cycle, reg_val);
-        }
         *current_cycle += 1;
+        let crt_length = crt_frame_buffer.len() as i32 % 40;
+        if crt_length >= reg_val-1 && crt_length <= reg_val+1 {
+            crt_frame_buffer.push('#');
+        } else {
+            crt_frame_buffer.push('.');
+        }
+        // println!("CRT: {:?}\nSprite: ", crt_frame_buffer);
+        for i in 0..40 {
+            if i >= reg_val -1 && i <= reg_val+1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        // println!("\n");
     }
-    if *current_cycle == pause_cycle {
-        signal_strength = pause_cycle as i32 * reg_val;
-        println!("Signal Strength: {}; Cycle Count: {}; X = {}", signal_strength, &current_cycle, reg_val);
+}
+
+fn print_crt(frame_buffer: &Vec<char>) {
+    for (i, letter) in frame_buffer.iter().enumerate() {
+        if i % 40 == 0 {
+            println!();
+        } 
+        print!("{letter}");
     }
-    signal_strength
+    println!();
+}
+
+fn clear_screen() {
+    print!("\x1b2J");
 }
